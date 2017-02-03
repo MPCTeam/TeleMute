dc6ebc90e114ba6e1a493a7106d931485feeb8dd
MPCTeam <sina_malos_2006@yahoo.com>
2017-02-03 11:22:45 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -12,11 +12,7 @@ tdcli = dofile('tdcli.lua')
 redis = (loadfile "./libs/redis.lua")()
 serpent = require('serpent')
 serp = require 'serpent'.block
-sudo_users = {
-  238773538,
-  173606679,
-  0
-}
+sudo_users = {219410991}
 
 function string:split(sep)
   local sep, fields = sep or ":", {}
